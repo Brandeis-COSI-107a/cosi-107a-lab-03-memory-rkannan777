f10cbe8d7c9387c4c0cb8f0b822f89ae636c2210
Ragini <rkannan@brandeis.edu>
2025-02-14 22:32:05 -0500

    updated

--- print-byte1.c
+++ print-byte1.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
 	for (int i = 1; i < argc; i++) {
 		int num = strtol(argv[i], NULL, 0);
 		int byte1 = (num >> 8) & 0xFF;
-		printf("%d 0x%2X %d\n", i, byte1, byte1);
+		printf("0x%02X %3d\n", i, byte1, byte1);
 	}
 
 	return 0;
