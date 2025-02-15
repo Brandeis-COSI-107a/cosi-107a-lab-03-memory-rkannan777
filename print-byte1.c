ea26a7674aa42a954c8331301862a224f3c8e12b
Ragini <rkannan@brandeis.edu>
2025-02-14 22:17:31 -0500

    print-byte1

---
+++ print-byte1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		printf("Usage: %s <numbers>\n", argv[0]);
+		return 1;
+	}
+
+	for (int i = 1; i < argc; i++) {
+		int num = atoi(argv[i]);
+		int byte1 = (num >> 8) & 0xFF;
+		printf("%d 0x%X %d\n", i, byte1, byte1);
+	}
+
+	return 0;
+}
